edd501d3798d6ad75b75514589867d859c165e25
Alex Santos <arkanjo.alex@gmail.com>
2021-06-01 23:38:28 -0300

    Adiciona controller vazio de de Atividade

---
+++ InfnetAtividadesComplementaresApi/Controllers/AtividadeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InfnetAtividadesComplementaresApi.Controllers
+{
+    [Route("api/atividade")]
+    [ApiController]
+    public class AtividadeController : ControllerBase
+    {
+        public AtividadeController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Atividade 01");
+        }
+    }
+}
