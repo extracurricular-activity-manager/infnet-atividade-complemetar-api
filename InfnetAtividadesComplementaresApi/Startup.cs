143ce5e9474adaa7d24d7cd5cfd072c49e43e394
Alex Santos <arkanjo.alex@gmail.com>
2021-06-01 23:38:10 -0300

    Adiciona Swagger

--- InfnetAtividadesComplementaresApi/Startup.cs
+++ InfnetAtividadesComplementaresApi/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace InfnetAtividadesComplementaresApi
 {
@@ -27,11 +20,13 @@ namespace InfnetAtividadesComplementaresApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,6 +36,18 @@ namespace InfnetAtividadesComplementaresApi
 
             app.UseRouting();
 
+            //Swagger
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "API - Gestão de Atividade Complementar");
+                c.RoutePrefix = "swagger";
+            });
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -48,7 +55,7 @@ namespace InfnetAtividadesComplementaresApi
                 endpoints.MapControllers();
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+                    await context.Response.WriteAsync("INFNET - Gerenciador de Atividades Complementares");
                 });
             });
         }
