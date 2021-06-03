36b7664772ce94d4abd447775ad472a9379d725d
Alex Santos <arkanjo.alex@gmail.com>
2021-06-03 16:17:36 -0300

    retorno com objetos mock na consulta

--- InfnetAtividadesComplementaresApi/Controllers/AtividadeController.cs
+++ InfnetAtividadesComplementaresApi/Controllers/AtividadeController.cs
@@ -1,8 +1,11 @@
 ﻿using InfnetAtividadesComplementaresApi.App.Domain.Atividades.Entity;
-using InfnetAtividadesComplementaresApi.App.Dto.Cmd;
+using InfnetAtividadesComplementaresApi.App.Domain.Atividades.Enum;
+using InfnetAtividadesComplementaresApi.App.Domain.Atividades.ValueObject;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Mime;
 
 namespace InfnetAtividadesComplementaresApi.Controllers
@@ -30,11 +33,11 @@ namespace InfnetAtividadesComplementaresApi.Controllers
         [ProducesResponseType(typeof(IEnumerable<Atividade>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult ConsultaDeAtividades([FromQuery] DocumentoCmd documento)
+        public ActionResult ConsultaDeAtividades([FromQuery][Required] string documento)
         {
             try
             {
-                return Ok(new Atividade());
+                return Ok(CriaMockListaDeAtividade());
             }
             catch (System.Exception ex)
             {
@@ -45,6 +48,84 @@ namespace InfnetAtividadesComplementaresApi.Controllers
                     new { informacao = ex.Message }
                 );
             }
+        }
+
+        private IList<Atividade> CriaMockListaDeAtividade()
+        {
+            var aluno = new Aluno()
+            {
+                AnoDeConcessao = 2018,
+                Documento = new Documento("12345678900"),
+                Matricula = "12345678900",
+                TotalHorasRealizadas = 127
+            };
+
+            var dataHoje = DateTime.Now;
+
+            return new List<Atividade>() {
+                new Atividade
+                {
+                    Aluno = aluno,
+                    Categoria = new Categoria
+                    {
+                        DescricaoRegraDeConcessao = "Descricao 2",
+                        LimiteDeHoras = 100
+                    },
+                    Avaliacao = new Avaliacao
+                    {
+                        DataDaAvaliacao = dataHoje.AddDays(2),
+                        HorasConcedidas = 133,
+                        NomeDoAvaliador = "Pivotto",
+                        Observacao = "Minha simples observação",
+                        Revisao = false
+                    },
+                    Revisao = null,
+                    CodigoSequencial = 132132,
+                    DataDaAtividade = dataHoje,
+                    DataDaSolicitacao = dataHoje,
+                    HorasSolicitadas = 50,
+                    JustificativaDaSolicitacao = "Só quero o canudo.",
+                    TipoDeComprovacao = TipoComprovacaoEnum.Certificado,
+                    TipoDeComprovacaoDescricao = TipoComprovacaoEnum.Certificado.ToString()
+                },
+                new Atividade
+                {
+                    Aluno = aluno,
+                    Categoria = new Categoria
+                    {
+                        DescricaoRegraDeConcessao = "Descricao 2",
+                        LimiteDeHoras = 100
+                    },
+                    Avaliacao = new Avaliacao
+                    {
+                        DataDaAvaliacao = dataHoje.AddDays(2),
+                        HorasConcedidas = 12,
+                        NomeDoAvaliador = "Pivotto",
+                        Observacao = "Minha simples observação",
+                        Revisao = false
+                    },
+                    Revisao = new Revisao()
+                    {
+                        AvaliacaoDaRevisao = new Avaliacao
+                        {
+                            DataDaAvaliacao = dataHoje,
+                            HorasConcedidas = 120,
+                            NomeDoAvaliador = "Pivoto dando Revisao",
+                            Observacao = "mlk se esforçou",
+                            Revisao = true
+                        },
+                        HorasSolicitadas = 120,
+                        Justificativa = "Vale o esforço"
+                    },
+                    CodigoSequencial = 132132,
+                    DataDaAtividade = dataHoje,
+                    DataDaSolicitacao = dataHoje,
+                    HorasSolicitadas = 10,
+                    JustificativaDaSolicitacao = "Só quero o canudo. 2",
+                    TipoDeComprovacao = TipoComprovacaoEnum.Declaracao,
+                    TipoDeComprovacaoDescricao = TipoComprovacaoEnum.Declaracao.ToString()
+                },
+            };
         }
     }
 }
