50bc808d389f0a3f0d42506b8f7d2440d412ff86
Alex Santos <arkanjo.alex@gmail.com>
2021-06-03 18:30:25 -0300

    classes de serviço de login

---
+++ InfnetAtividadesComplementaresApi/App/Application/Login/ServicoDeLogin.cs
@@ -0,0 +1,13 @@
+﻿using InfnetAtividadesComplementaresApi.App.Domain.GerenciaDeUsuario.Entity.Interface;
+
+namespace InfnetAtividadesComplementaresApi.App.Application.Login
+{
+    public class ServicoDeLogin : IServicoDeLogin
+    {
+        public ServicoDeLogin()
+        {
+
+        }
+
+    }
+}
